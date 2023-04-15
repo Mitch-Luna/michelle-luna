a192f404c3139a628ab3701085833b917042d884
mitch-luna <lunitamitch02@gmail.com>
2023-04-15 14:40:04 -0600

    creando crud de eccomerce

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductosModule } from './productos/productos.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
-  imports: [],
+  imports: [ProductosModule, CategoriasModule],
   controllers: [AppController],
   providers: [AppService],
 })
